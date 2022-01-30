4e173c623672ef86897885b192dd5292c0f49fc3
bogor <bogor@DESKTOP-DC6RO5J>
2022-01-30 16:23:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+
+    public static void Main()
+    {
+        int a1;
+       
+        a1 = new int[2, 2]
+        {
+           { 3, -14 }, { 11, 7 }
+        }
+
+        .Cast<int>().Aggregate(1, (y, x) => 0 < x && x < 10 ? y * x : y);
+        Console.WriteLine(a1);
+    }
+    
+}
